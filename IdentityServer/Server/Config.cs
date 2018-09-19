9128a313cf77cfc1114b55b439f184143dec04ef
Shiv Tyagi <shiv@imentor.co.in>
2018-09-19 12:44:58 +0530

    IdentityServer - Added in memory server

---
+++ IdentityServer/Server/Config.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.Extensions.Configuration;
+
+namespace Server
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("test-api", "Test Api"),
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client()
+                {
+                    ClientId = "test-api",
+                    ClientSecrets = new[] {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new[] {"test-api"}
+                },
+            };
+        }
+
+        public static IEnumerable<TestUser> GetUsers()
+        {
+            return new[]
+            {
+                new TestUser()
+                {
+                    SubjectId = "<Random-UID>-1",
+                    Username = "admin",
+                    Password = "admin"
+                },
+            };
+        }
+    }
+}
---
+++ IdentityServer/Server/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Server
+{
+    public class Startup
+    {
+        public string ContentRoot => Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+            AppSettings = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public IConfiguration Configuration { get; }
+        public static IConfiguration AppSettings { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddIdentityServer()
+                .AddDeveloperSigningCredential()    // temporay sign-in credentials
+                .AddTestUsers(Config.GetUsers().ToList())   // add users
+                .AddInMemoryClients(Config.GetClients())    // add clients
+                .AddInMemoryApiResources(Config.GetApiResources())  // add resources
+                ;
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory factory)
+        {
+            factory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //tells the middleware to use identity server
+            app.UseIdentityServer();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
